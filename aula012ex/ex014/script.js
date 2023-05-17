04ea668f4720d4227e9030719ac37f747ab60fda
leandrob0t <125820074+leandrob0t@users.noreply.github.com>
2023-05-17 14:43:40 -0300

    att

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -1,1 +1,15 @@
-alert('Olá');
+function carregar(){
+    var msg = document.getElementById('msg');
+    var img = document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manhap.png'; 
+    }else if (hora > 12 && hora <= 18){
+        img.src = 'tardep.png';
+    }else {
+        img.src = 'noitep.png';
+    }
+}
+
