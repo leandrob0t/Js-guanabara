fc499a41aecba784ce4e87fb2de3225194cd6b21
leandrob0t <125820074+leandrob0t@users.noreply.github.com>
2023-05-20 15:25:33 -0300

    att

---
+++ aula014/ambiente.js
@@ -0,0 +1,10 @@
+//var c =1;
+// while(c <=5) {
+//     console.log(c)
+//     c =c+1 //c++
+// }
+console.log('Vai começar...')
+for(var c=1; c <=5; c++) {
+    console.log(c)
+}
+console.log('Fim...')
