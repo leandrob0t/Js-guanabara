d6c02c4bd9fa19eb4e4cd771fd4a5635df8f4db8
leandrob0t <125820074+leandrob0t@users.noreply.github.com>
2023-05-20 14:27:38 -0300

    att

---
+++ aula013/ambiente.js
@@ -0,0 +1,18 @@
+// var cont = 1;
+// while(cont <=6) {
+//     console.log('Tudo bem?')
+//     cont++ // c=c+1
+// }
+
+// var cont = 1;
+// do{
+//     console.log(`Tudo bem? ${cont}`)
+//     cont++ // c=c+1
+// }while(cont <=6)
+
+var cont = 10;
+while(cont >0){
+    cont--
+    console.log(`${cont}`)
+}
+
