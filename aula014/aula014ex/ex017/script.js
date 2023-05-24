d84c85266b27d2a6832d92a4e0756d5def0e78cc
leandrob0t <125820074+leandrob0t@users.noreply.github.com>
2023-05-24 16:14:51 -0300

    att

--- aula014/aula014ex/ex017/script.js
+++ aula014/aula014ex/ex017/script.js
@@ -1,1 +1,21 @@
-alert('Olá');
+
+function tabuada(){
+    let num = document.getElementById('numtxt');
+    let tab = document.getElementById('seltab');
+    if (num.value.lenght == 0){
+        alert('Por favor, Digite um número!!')
+    }else {
+        let n = Number(num.value);
+        let c = 1;
+        tab.innerHTML = '';
+        while (c<=10){
+            let item = document.createElement('option');
+            item.text = `${n} X ${c} = ${n*c}`;
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+    
+
+}
